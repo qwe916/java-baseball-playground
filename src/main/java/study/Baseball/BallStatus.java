7438698988d71e2f49708695d6446cff76cca7b5
qwe916 <qwe916@naver.com>
2023-07-16 01:47:36 +0900

    feat: Ball 판정 결과를 나타내는 BallStatus

---
+++ src/main/java/study/Baseball/BallStatus.java
@@ -0,0 +1,13 @@
+package study.Baseball;
+
+public enum BallStatus {
+    STRIKE, BALL, NOTHING;
+
+    public boolean isStrike() {
+        return this == STRIKE;
+    }
+
+    public boolean isBall() {
+        return this == BALL;
+    }
+}
